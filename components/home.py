32692178f08aba066ae67f745a831d5e2953a295
Pedro Cisdeli <pedrohcsideli@usp.br>
2023-07-20 11:45:09 -0500

    Added dbc to home page and changed dropdown text alignment

--- components/home.py
+++ components/home.py
@@ -1,58 +1,88 @@
 from dash import dcc, html, dash_table
+import dash_bootstrap_components as dbc
 
-layout = html.Div(
+layout = dbc.Container(
     [
-        html.H1('Table'),
-        dash_table.DataTable(
-            id='table-data',
-            page_size=5,
-            style_data_conditional=[
-                {
-                    'if': {'row_index': 'even'},
-                    'backgroundColor': 'rgb(220, 220, 220)',
+        dbc.Row([
+            dbc.Row([
+                html.H1(
+                    'Yield per County by Year',
+                    style={'text-align': 'center'}
+                )
+            ]),
+            dbc.Row([
+                dcc.Dropdown(
+                    id='yield-county-year-dropdown',
+                    clearable=False,
+                )
+            ]),
+            dbc.Row([
+                dbc.Col([dcc.Graph(id='yield-county-graph')], width=6),
+                dbc.Col([dcc.Graph(id='yield-county-map')], width=6),
+            ]),
+        ]),
+
+        dbc.Row([
+            dbc.Row([
+                html.H1(
+                    'Table',
+                    style={'text-align': 'center'}
+                )
+            ]),
+            dash_table.DataTable(
+                id='table-data',
+                page_size=5,
+                style_data_conditional=[
+                    {
+                        'if': {'row_index': 'even'},
+                        'backgroundColor': 'rgb(220, 220, 220)',
+                    }
+                ],
+                style_cell={
+                    'text-align': 'left'
                 }
-            ],
-            style_cell={
-                'text-align': 'left'
-            }
-        ),
-
-        html.H1('Mean Yield per County by Year'),
-        html.Div([
-            html.H3('Year:', style={'margin-right': '10px'}),
-            dcc.Dropdown(
-                id='yield-county-year-dropdown',
-                clearable=False,
-                style={'margin-right': '30px'}
             ),
-        ], style={'display': 'flex'}),
-        dcc.Graph(id='yield-county-graph'),
+        ]),
 
-        html.H1('Mean Brand Yield per Year'),
-        html.Div([
-            html.H3('Brand:', style={'margin-right': '10px'}),
-            dcc.Dropdown(
-                id='brand-year-dropdown',
-                clearable=False,
-                style={'margin-right': '150px'}
-            ),
-        ], style={'display': 'flex'}),
-        dcc.Graph(id='brand-year-graph'),
+        dbc.Row([
+            dbc.Row([
+                html.H1(
+                    'Mean Brand Yield per Year',
+                    style={'text-align': 'center'}
+                )
+            ]),
+            dbc.Row([
+                dcc.Dropdown(
+                    id='brand-year-dropdown',
+                    clearable=False,
+                )
+            ]),
+            dcc.Graph(id='brand-year-graph'),
+        ]),
 
-        html.H1('Yield per Brand'),
-        html.Div([
-            html.H3('Year:', style={'margin-right': '10px'}),
+        dbc.Row([
+            dbc.Row([
+                html.H1(
+                    'Yield per Brand',
+                    style={'text-align': 'center'}
+                )
+            ]),
             dcc.Dropdown(
                 id='yield-brand-dropdown',
                 clearable=False,
-                style={'margin-right': '30px'}
             ),
-        ], style={'display': 'flex'}),
-        dcc.Graph(id='yield-brand-graph'),
+            dcc.Graph(id='yield-brand-graph'),
+        ]),
 
-        html.H1('Mean Yield per Year'),
-        dcc.Graph(id='yield-year-graph'),
-
-
-    ]
+        dbc.Row([
+            dbc.Row([
+                html.H1(
+                    'Mean Yield per Year',
+                    style={'text-align': 'center'}
+                )
+            ]),
+            dcc.Graph(id='yield-year-graph'),
+        ]),
+    ],
+    fluid=True
 )
