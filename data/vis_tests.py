bb33a2e10a8f1ffc2509194e0ed0a4d8d48f126c
cisdeli <pedrohcisdeli@gmail.com>
2024-02-23 17:09:18 -0600

    Better Yield by Env_mena plot

--- data/vis_tests.py
+++ data/vis_tests.py
@@ -52,46 +52,35 @@ def env_mean(df):
     _df = df.copy()
     _df["ENV_MEAN"] = _df.groupby(["YEAR", "COUNTY_CITY"])["YIELD"].transform("mean")
     _df = (
-        # _df[df["BRAND"].isin(["DEKALB", "FRONTIER", "PIONEER"])]
-        _df[df["BRAND"].isin(["DEKALB"])]
-        .groupby(["YEAR", "BRAND", "ENV_MEAN"])["YIELD"]
+        _df[df["BRAND"].isin(["DEKALB", "FRONTIER", "PIONEER"])]
+        .groupby(["BRAND", "ENV_MEAN", "WATER_REGIME"])["YIELD"]
         .mean()
         .reset_index()
     )
-    print(_df)
+
     fig = px.scatter(
         _df,
         x="ENV_MEAN",
-        # x="YEAR",
         y="YIELD",
-        color="YEAR",
-        title="Dekalb Yield x Env Mean thoughout the years",
+        color="WATER_REGIME",
+        facet_col="BRAND",
+        title="Brand Yield x Env Mean thoughout the years",
     )
 
-    # Add a horizontal threshold line
     fig.add_shape(
         type="line",
         x0=min(_df["ENV_MEAN"]),
         x1=max(_df["ENV_MEAN"]),
         y0=min(_df["ENV_MEAN"]),
         y1=max(_df["ENV_MEAN"]),
-        line=dict(color="black", width=2, dash="dash"),  # Customize line appearance
+        line=dict(color="black", width=2, dash="dash"),  
+        row="all",
+        col="all",
     )
 
-    fig.update_traces(marker={'size': 15})
-    # fig = px.scatter(
-    #     _df,
-    #     x="ENV_MEAN",
-    #     y="YIELD",
-    #     color="BRAND",
-    #     trendline="lowess",
-    #     trendline_options=dict(frac=0.666),
-    #     title="Yearly Yield Trend",
-    # )
-    #
-    # fig.data = [t for t in fig.data if t.mode == "lines"]
-    #
-    # fig.update_traces(showlegend=True)
+    fig.update_traces(marker={"size": 10})
+    fig.for_each_annotation(lambda a: a.update(text=a.text.replace("Name=", "")))
+
     fig.show()
 
 
